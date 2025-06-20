e3ad50de969f7267b830fc69a772108a3fe9380f
Phadon Makara <phadonmakara@gmail.com>
2025-06-21 01:46:28 +0700

    add functional sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,39 @@
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './Context'
+import sublinks from './data'
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+  return (
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <div className='sidebar-container'>
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        {/* iterating through the main links */}
+        {sublinks.map((item) => {
+          const { links, page, pageId } = item
+          return (
+            <article key={pageId}>
+              <h4>{page}</h4>
+              <div className='sidebar-sublinks'>
+                {/* iterating through the sublinks */}
+                {links.map((link) => {
+                  const { url, icon, label, id } = link
+                  return (
+                    <a href={url} key={id}>
+                      {icon}
+                      {label}
+                    </a>
+                  )
+                })}
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </aside>
+  )
 }
+
 export default Sidebar
