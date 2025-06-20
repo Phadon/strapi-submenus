79ae5bab913d02684509736ee70b885d6fd6dcbd
Phadon Makara <phadonmakara@gmail.com>
2025-06-21 01:23:10 +0700

    added navbar, hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useGlobalContext } from './Context'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Submenu from './Submenu'
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
-};
-export default App;
+  const { isSidebarOpen } = useGlobalContext()
+  console.log(isSidebarOpen)
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  )
+}
+export default App
